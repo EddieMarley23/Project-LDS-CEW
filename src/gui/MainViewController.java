eb69082cbb55eb03e91e22d2067872d3fdabb034
Eddie Marley <marley_eddie@hotmail.com>
2024-08-26 20:32:18 -0300

    DepartmentList View

--- src/gui/MainViewController.java
+++ src/gui/MainViewController.java
@@ -1,11 +1,13 @@
 package gui;
 
+
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 import application.Main;
 import gui.util.Alerts;
+import javafx.application.Application;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
@@ -15,56 +17,58 @@ import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.MenuItem;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
 
-public class MainViewController implements Initializable {
 
-	@FXML
-	private MenuItem menuItemCar;
-	@FXML
-	private MenuItem menuItemModel;
-	@FXML
-	private MenuItem menuItemAbout;
 
-	@FXML
-	public void onMenuItemCarAction() {
-		System.out.println("onMenuItemCarAction");
-	}
+	public class MainViewController implements Initializable {
 
-	@FXML
-	public void onMenuItemModelAction() {
-		System.out.println("onMenuItemModelAction");
-	}
+		@FXML
+		private MenuItem menuItemSeller;
+		@FXML
+		private MenuItem menuItemDepartment;
+		@FXML
+		private MenuItem menuItemAbout;
 
-	@FXML
-	public void onMenuItemAboutAction() {
-		loadView("/gui/About.fxml");
-	}
-
-	@Override
-	public void initialize(URL uri, ResourceBundle rb) {
-		
-
-	}
-
-	private synchronized void loadView(String absoluteName) {
-		try {
-		FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
-		VBox newBox = loader.load();
-		
-		Scene mainScene = Main.getMainScene();
-		VBox mainVBox = (VBox) ((ScrollPane) mainScene.getRoot()).getContent();
-		
-		Node mainMenu = mainVBox.getChildren().get(0);
-		mainVBox.getChildren().clear();
-		mainVBox.getChildren().add(mainMenu);
-		mainVBox.getChildren().addAll(newBox.getChildren());
-		
+		@FXML
+		public void onMenuItemSellerAction() {
+			System.out.println("onMenuItemSellerAction");
 		}
-		catch(IOException e) {
-			Alerts.showAlert("IO Excpetion", "Error loading view", e.getMessage(),AlertType.ERROR);
+
+		@FXML
+		public void onMenuItemDepartmentAction() {
+			loadView("/gui/DepartmentList.fxml");
+		}
+
+		@FXML
+		public void onMenuItemAboutAction() {
+			loadView("/gui/About.fxml");
+		}
+
+		@Override
+		public void initialize(URL uri, ResourceBundle rb) {
+
+		}
+
+		private synchronized void loadView(String absoluteName) {
+			try {
+				FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
+				VBox newBox = loader.load();
+
+				Scene mainScene = Main.getMainScene();
+				VBox mainVBox = (VBox) ((ScrollPane) mainScene.getRoot()).getContent();
+
+				Node mainMenu = mainVBox.getChildren().get(0);
+				mainVBox.getChildren().clear();
+				mainVBox.getChildren().add(mainMenu);
+				mainVBox.getChildren().addAll(newBox.getChildren());
+
+			} catch (IOException e) {
+				Alerts.showAlert("IO Excpetion", "Error loading view", e.getMessage(), AlertType.ERROR);
+			}
 		}
 	}
-}
-		
+
+
 
 
